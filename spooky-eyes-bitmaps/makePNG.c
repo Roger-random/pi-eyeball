968b6e21dad8ccffd5ab454825ea9b02e829c2b1
Roger Cheng <roger.random@outlook.com>
2019-02-05 16:53:42 -0800

    PNG converter v1.0 complete

--- spooky-eyes-bitmaps/makePNG.c
+++ spooky-eyes-bitmaps/makePNG.c
@@ -18,24 +18,26 @@
 #include <png.h>
 #include <stdint.h>
 
-#include "defaultEye.h"
+#include "defaultEye.h" // Replace with appropriate header for different eye
 
 // This function actually writes out the PNG image file. The string 'title' is
 // also written into the image file
 int writeImage(char* filename, int width, int height, png_bytep* buffer, char* title);
 
+// Allocate a buffer of width * height pixels, each pixel consists of 3 png_bytep
 png_bytep* allocPNGBuffer(int width, int height)
 {
 	png_bytep* row_pointers = (png_bytep*)malloc(height*sizeof(png_bytep));
 	int row, column;
 	for (row=0; row < height; row++)
 	{
-		row_pointers[row] = (png_bytep)malloc(3*width); // Each pixel has three bytes for RGB
+		row_pointers[row] = (png_bytep)malloc(3*width*sizeof(png_bytep)); // Each pixel has three bytes for RGB
 	}
 
 	return row_pointers;
 }
 
+// Free array allocated by the above
 void freePNGBuffer(int height, png_bytep* buffer)
 {
 	for (int row=0; row < height; row++)
@@ -45,6 +47,7 @@ void freePNGBuffer(int height, png_bytep* buffer)
 	free(buffer);
 }
 
+// Use sclera array in header file to write out sclera.png
 void writeSclera()
 {
 	png_bytep* row_pointers = allocPNGBuffer(SCLERA_WIDTH, SCLERA_HEIGHT);
@@ -57,6 +60,7 @@ void writeSclera()
 		{
 			uint16_t rgb565 = sclera[row][column];
 
+			// Shift bits to turn 16-bit RGB565 value into 24-bit RGB888
 			row_pointers[row][column*3  ] = (rgb565 >> 8) & 0xF8;
 			row_pointers[row][column*3+1] = (rgb565 >> 3) & 0xFC;
 			row_pointers[row][column*3+2] = (rgb565 << 3) & 0xF8;
@@ -69,11 +73,91 @@ void writeSclera()
 	freePNGBuffer(SCLERA_HEIGHT, row_pointers);
 }
 
+// Repeat for iris
+void writeIris()
+{
+	png_bytep* row_pointers = allocPNGBuffer(IRIS_MAP_WIDTH, IRIS_MAP_HEIGHT);
+
+	printf("Reading iris array\n");
+	int row, column;
+	for (row=0; row<IRIS_MAP_HEIGHT; row++)
+	{
+		for(column=0; column<IRIS_MAP_WIDTH; column++)
+		{
+			uint16_t rgb565 = iris[row][column];
+
+			row_pointers[row][column*3  ] = (rgb565 >> 8) & 0xF8;
+			row_pointers[row][column*3+1] = (rgb565 >> 3) & 0xFC;
+			row_pointers[row][column*3+2] = (rgb565 << 3) & 0xF8;
+		}
+	}
+
+	printf("Writing iris.png\n");
+	writeImage("iris.png", IRIS_MAP_WIDTH, IRIS_MAP_HEIGHT, row_pointers, "Iris");
+
+	freePNGBuffer(IRIS_MAP_HEIGHT, row_pointers);
+}
+
+// Write PNG representation of grayscale upper eyelid
+void writeUpper()
+{
+	png_bytep* row_pointers = allocPNGBuffer(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	printf("Reading upper eyelid array\n");
+	int row, column;
+	for (row=0; row<SCREEN_HEIGHT; row++)
+	{
+		for(column=0; column<SCREEN_WIDTH; column++)
+		{
+			uint8_t grayscale = upper[row][column];
+
+			row_pointers[row][column*3  ] = grayscale;
+			row_pointers[row][column*3+1] = grayscale;
+			row_pointers[row][column*3+2] = grayscale;
+		}
+	}
+
+	printf("Writing upper.png\n");
+	writeImage("upper.png", SCREEN_WIDTH, SCREEN_HEIGHT, row_pointers, "Upper eyelid");
+
+	freePNGBuffer(SCREEN_HEIGHT, row_pointers);
+}
+
+// Repeat for lower eyelid
+void writeLower()
+{
+	png_bytep* row_pointers = allocPNGBuffer(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	printf("Reading lower eyelid array\n");
+	int row, column;
+	for (row=0; row<SCREEN_HEIGHT; row++)
+	{
+		for(column=0; column<SCREEN_WIDTH; column++)
+		{
+			uint8_t grayscale = lower[row][column];
+
+			row_pointers[row][column*3  ] = grayscale;
+			row_pointers[row][column*3+1] = grayscale;
+			row_pointers[row][column*3+2] = grayscale;
+		}
+	}
+
+	printf("Writing lower.png\n");
+	writeImage("lower.png", SCREEN_WIDTH, SCREEN_HEIGHT, row_pointers, "Lower eyelid");
+
+	freePNGBuffer(SCREEN_HEIGHT, row_pointers);
+}
+
+// Main function writes out four spooky eye arrays into their individual PNG files
 int main(int argc, char *argv[])
 {
 	writeSclera();
+	writeIris();
+	writeUpper();
+	writeLower();
 }
 
+// Mostly untouched from original libPNG example
 int writeImage(char* filename, int width, int height, png_bytep* buffer, char* title)
 {
 	int code = 0;
